a7e8698f449731047a9bed04fe66e3981fb5886d
Pallavi Kumari <75251279+pallavicops@users.noreply.github.com>
2024-09-17 20:45:11 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+
 import { Container, Row, Col, Button, Spinner } from 'react-bootstrap';
 import './App.css';
 import Header from './components/Header';
